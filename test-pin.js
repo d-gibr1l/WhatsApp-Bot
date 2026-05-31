69552a51927e94eaba740cb0bd24597dadf18fce
d-gibr1l <dominicgibril@gmail.com>
2026-05-31 11:33:51 +0000

    chore: remove irrelevant test scripts, output logs, and editor backup files

--- test-pin.js
+++
@@ -1,6 +0,0 @@
-fetch('https://www.pinterest.com/search/pins/?q=superman', { headers: { 'User-Agent': 'Mozilla/5.0' } })
-  .then(res => res.text())
-  .then(html => {
-    const urls = [...html.matchAll(/https:\/\/i\.pinimg\.com\/[a-zA-Z0-9_\/-]+\.jpg/g)].map(m => m[0]);
-    console.log(urls.slice(0, 10));
-  });
