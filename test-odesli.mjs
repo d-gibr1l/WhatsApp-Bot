39efbcf4456d50cf9d8315c153524d531b6cc0ca
d-gibr1l <dominicgibril@gmail.com>
2026-06-02 17:58:47 +0000

    chore: remove test scripts

--- test-odesli.mjs
+++
@@ -1,6 +0,0 @@
-async function test() {
-  const res = await fetch("https://api.song.link/v1-alpha.1/links?url=https://open.spotify.com/track/6VnF7N8Tz1wU4EeeLPR3uC");
-  const data = await res.json();
-  console.log(JSON.stringify(data, null, 2));
-}
-test();
