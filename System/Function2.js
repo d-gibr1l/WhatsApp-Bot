77bb3a35628a99efd3fd369929dc5b99070020dc
d-gibr1l <dominicgibril@gmail.com>
2026-08-12 23:08:40 +0000

    Fix jimp import: use default export for v0.16.x

--- System/Function2.js
+++ System/Function2.js
@@ -6,7 +6,7 @@ import axios from "axios";
 import moment from "moment-timezone";
 import { sizeFormatter } from "human-readable";
 import util from "util";
-import { Jimp } from "jimp";
+import Jimp from "jimp";
 import child_process from "child_process";
 import ffmpegStatic from "ffmpeg-static";
 
